d22ee4b22c958ec21f992332d1d0a249321362c4
C1316943 Hugo Fagundes de Sousa <c1316943@interno.bb.com.br>
2021-07-06 14:59:38 -0300

    initial commit

--- index.tsx
+++ index.tsx
@@ -1,3 +1,10 @@
 import SearchLocation from './src/SearchLocation';
+import FullButton from './src/FullButton';
+import PopUp from './src/PopUp';
+import ProgressBar from './src/ProgressBar';
 
-export { SearchLocation };
+export { 
+    SearchLocation,
+    FullButton,
+    PopUp,
+    ProgressBar, };
